82a308d34d9b232a6d0f8e0527c4e92b498ccaaa
Nitheshkum <128707052+Nitheshkum@users.noreply.github.com>
2023-03-23 19:21:36 +0530

    Update stringReverse.java

--- stringReverse.java
+++ stringReverse.java
@@ -1,1 +1,17 @@
+class stringReverse{
+  public static void main(String args[]){
+    Scanner in=new Scanner(System.in);
+    StringBuilder input=in.nextline();
+    StringBuilder result=new StringBuilder();
+    for(int i=input.lenght()-1;i>=0;i--){
+      result.append(input.charAt(i));
+      }
+    System.out.println("Reversed ouput"+result);
+    }
+  
+  }
+
+
+
+
 
