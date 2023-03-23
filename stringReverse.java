77a0aee8f2f26336870dee6197fdb1dbd8037e55
Nitheshkum <128707052+Nitheshkum@users.noreply.github.com>
2023-03-23 20:28:29 +0530

    Update stringReverse.java

--- stringReverse.java
+++ stringReverse.java
@@ -1,16 +1,15 @@
+import java.util.Scanner;
 class stringReverse{
   public static void main(String args[]){
-    Scanner in=new Scanner(System.in);
-    StringBuilder input=in.nextline();
-    StringBuilder result=new StringBuilder();
-    for(int i=input.lenght()-1;i>=0;i--){
-      result.append(input.charAt(i));
-      }
-    System.out.println("Reversed ouput"+result);
-    }
-  
+    String a;
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter the string");
+    a=sc.nextLine();
+    System.out.println("THE STRING IS");
+    StringBuilder b=new StringBuilder(a);
+    System.out.println(b.reverse());
   }
-
+}
 
 
 
